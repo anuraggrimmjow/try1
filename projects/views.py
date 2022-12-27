4d1d41ede8a1a49e966ec79067343a0961055290
anuraggrimmjow <121508101+anuraggrimmjow@users.noreply.github.com>
2022-12-27 21:41:25 +0530

    Add files via upload

---
+++ projects/views.py
@@ -0,0 +1,54 @@
+from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from projects.models import Projects,Info,About
+from collections import OrderedDict
+
+# Create your views here.
+
+
+def index(request):
+    # print("check 1")
+
+    projectss=Projects.objects.all()
+    projectInfo={}
+
+    for x in projectss:
+        projectInfo[x.priority]={"firstLine":x.firstLine,"description":x.description,"github":x.github,"image":x.images}
+
+    print(projectInfo)
+    projectInfo = OrderedDict(sorted(projectInfo.items()))
+    
+    infos=Info.objects.all()
+    skills={}
+
+    for x in infos:
+        skills[x.priority]=x.skill
+
+    print(skills)
+    skills = OrderedDict(sorted(skills.items()))
+
+    abouts=About.objects.all()
+    firstLine=""
+    description=""
+
+    for x in abouts:
+        if x.priority==1:
+            firstLine=x.firstLine
+            description=x.description
+            break
+    print("sending")
+    
+
+    context = {
+    'projectInfo':projectInfo,
+        'skills':skills,
+        'firstLine':firstLine,
+        'description':description,
+}
+
+    return render(request,'index.html',context=context)
+
+      
+
+   
